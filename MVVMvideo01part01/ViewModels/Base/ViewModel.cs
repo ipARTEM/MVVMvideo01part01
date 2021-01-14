065106bc670022d7ad3deeb792ead5ec932a3ac9
Artem <a.khimin@yandex.ru>
2021-01-14 23:34:52 +0300

    Тест привязки к главному окну

---
+++ MVVMvideo01part01/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace MVVMvideo01part01.ViewModels.Base
+{
+    /// <summary>
+    /// Базовый класс   ViewModel   Модели представления
+    /// </summary>
+    internal abstract class ViewModel : INotifyPropertyChanged, IDisposable
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private bool _Disposed;
+        protected virtual void Dispose(bool Disposing)
+        {
+            if (!Disposing || _Disposed) return;
+            _Disposed = true;
+            // Освобождение управляемых ресурсов
+        }
+
+    }
+}
---
+++ MVVMvideo01part01/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,47 @@
+﻿using MVVMvideo01part01.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVVMvideo01part01.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+
+        #region Заголовок окна
+
+        private string _Title = "Тест Core";
+
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _Title;
+            //set
+            //{
+            //    //if (Equals(_Title, value)) return;
+            //    //_Title = value;
+            //    //OnPropertyChanged();
+
+            //    Set(ref _Title, value);
+            //}
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
+
+
+        #region  Статус программы
+        /// <summary>
+        /// Статус программы
+        /// </summary>
+        private string _Status = "Готов!!";
+
+        public string Status
+        {
+            get { return _Status; }
+            set { _Status = value; }
+        }
+        #endregion
+
+    }
+}
