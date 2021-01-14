8e502f89d752109ac1a5468d3f6bbb9b3041b866
Artem <a.khimin@yandex.ru>
2021-01-15 00:14:43 +0300

    Создана структура команд

---
+++ MVVMvideo01part01/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,28 @@
+﻿using MVVMvideo01part01.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVVMvideo01part01.Infrastructure.Commands
+{
+    internal class LambdaCommand : Command
+    {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+
+        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter)
+        {
+            if (!CanExecute(parameter)) return;
+            _Execute(parameter);
+        }
+    }
+
+}
