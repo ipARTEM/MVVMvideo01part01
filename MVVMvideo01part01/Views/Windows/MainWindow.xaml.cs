dfa11fea9289fd2e05e866b3769af3d5d8ea46b5
Artem <a.khimin@yandex.ru>
2021-01-14 22:25:09 +0300

    Создание папок

--- MVVMvideo01part01/Views/Windows/MainWindow.xaml.cs
+++ MVVMvideo01part01/Views/Windows/MainWindow.xaml.cs
@@ -2,9 +2,7 @@
 
 namespace MVVMvideo01part01
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+ 
     public partial class MainWindow : Window
     {
         public MainWindow()=>InitializeComponent();
