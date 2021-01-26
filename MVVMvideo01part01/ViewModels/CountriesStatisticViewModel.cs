eb44058fbcc92e6083e534f259fddf53f7b66e17
Artem <a.khimin@yandex.ru>
2021-01-27 00:20:45 +0300

    Базовая логика модели представления статистики

--- MVVMvideo01part01/ViewModels/CountriesStatisticViewModel.cs
+++ MVVMvideo01part01/ViewModels/CountriesStatisticViewModel.cs
@@ -1,17 +1,58 @@
-﻿using MVVMvideo04part04.ViewModels.Base;
+﻿using MVVMvideo04part04.Services;
+using MVVMvideo04part04.ViewModels.Base;
+using MVVMvideo04part04.Infrastructure.Commands;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using MVVMvideo04part04.Models;
 
 namespace MVVMvideo04part04.ViewModels
 {
-    internal class CountriesStatisticViewModel:ViewModel
+    internal class CountriesStatisticViewModel : ViewModel
     {
-        private readonly MainWindowViewModel _MainModel;
+        private DataService _DataService;
+
+        private MainWindowViewModel MainModel { get; }
+
+        #region Статистика по странам
+
+        /// <summary>Статистика по странам</summary>
+        private IEnumerable<CountryInfo> _Countries;
+
+        /// <summary>Статистика по странам</summary>
+        public IEnumerable<CountryInfo> Countries 
+        {
+            get => _Countries;
+            private set => Set(ref _Countries, value);
+        }
+
+        #endregion
+
+
+        #region Команды
+
+        public ICommand RefreshDataCommand { get; }
+
+        private void OnRefreshDataCommandExecuted(object p)
+        {
+            Countries = _DataService.GetData();
+        }
+
+        #endregion
+
 
         public CountriesStatisticViewModel(MainWindowViewModel MainModel)
         {
-            _MainModel = MainModel;
+            this.MainModel = MainModel;
+
+            _DataService = new DataService();
+
+            #region Команды
+
+            RefreshDataCommand = new LambdaCommand(OnRefreshDataCommandExecuted);
+
+            #endregion
         }
 
     }
