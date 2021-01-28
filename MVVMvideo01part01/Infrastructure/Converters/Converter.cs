e3ac940c939944a1607d4749e546f2f086336021
Artem <a.khimin@yandex.ru>
2021-01-28 20:36:04 +0300

    Перед заменой конвертеров на базовый класс

--- MVVMvideo01part01/Infrastructure/Converters/Converter.cs
+++ MVVMvideo01part01/Infrastructure/Converters/Converter.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
+using System.Windows.Data;
 
 namespace MVVMvideo04part04.Infrastructure.Converters
 {
-    class Converter
+    internal abstract class Converter : IValueConverter
     {
+        public abstract object Convert(object v, Type t, object p, CultureInfo c)
+        {
+           
+        }
+
+        public virtual object ConvertBack(object v, Type t, object p, CultureInfo c)
+        {
+            throw new NotSupportedException("Обратное преобразование не поддерживается");
+        }
     }
 }
