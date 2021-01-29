bcdb3eb7d924774b2201ba2108c3bf20b8bfcae3
Artem <a.khimin@yandex.ru>
2021-01-30 01:21:31 +0300

    Сделан Мультиконвертер

---
+++ MVVMvideo01part01/Infrastructure/Converters/MultiConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace MVVMvideo04part04.Infrastructure.Converters
+{
+    internal abstract class MultiConverter : IMultiValueConverter
+    {
+        public abstract object Convert(object[] vv, Type t, object p, CultureInfo c);  
+
+
+        public object[] ConvertBack(object v, Type[] tt, object p, CultureInfo c)
+        {
+            throw new NotSupportedException("Обратное преобразование не поддерживается");
+        }
+    }
+}
---
+++ MVVMvideo01part01/Infrastructure/Converters/ToArray.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace MVVMvideo04part04.Infrastructure.Converters
+{
+    internal class ToArray : MultiConverter
+    {
+        public override object Convert(object[] vv, Type t, object p, CultureInfo c)
+        {
+            var collection= new CompositeCollection();
+
+            foreach (var value  in vv)
+                collection.Add(value);
+            return collection;
+        }
+        //public override object[] ConvertBack(object v, Type[] tt, object p, CultureInfo c) =>v as object[];
+
+    }
+}
