339149c6be5b8b8a816d355a3117eeaa0c7e065a
Artem <a.khimin@yandex.ru>
2021-01-29 16:16:37 +0300

    Работает конвертер с отрицательным значением К

--- MVVMvideo01part01/Infrastructure/Converters/CompositeConverter.cs
+++ MVVMvideo01part01/Infrastructure/Converters/CompositeConverter.cs
@@ -17,14 +17,14 @@ namespace MVVMvideo04part04.Infrastructure.Converters
         public override object Convert(object v, Type t, object p, CultureInfo c)
         {
             var result1 = First?.Convert(v, t, p, c) ?? v;
-            var result2 = Second?.Convert(c,t, p, c) ?? result1;
+            var result2 = Second?.Convert(result1,t, p, c) ?? result1;
 
             return result2;
         }
 
         public override object ConvertBack(object v, Type t, object p, CultureInfo c)
         {
-            var result2=Second.ConvertBack(c, t, p, c) ?? v;
+            var result2=Second.ConvertBack(v, t, p, c) ?? v;
             var result1 = First?.ConvertBack(result2, t, p, c) ?? result2;
 
             return result1;
